d33f70d186cbcbf6eb09744c099de9e39abd6b5e
MalinaliRap <utfprdiario@gmail.com>
2023-04-11 18:36:22 -0300

    Aula #23

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import React from 'react';
 import styles from './ProjectForm.module.css';
 import Input from '../form/Input';
@@ -5,6 +6,26 @@ import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(
+        () => {
+            fetch("http://localhost:5000/categories", {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data);
+            })
+            .catch((err) => console.log(err));
+        }, []
+    )
+
+
     return (
         <form className={styles.form}>
             <Input 
@@ -22,6 +43,7 @@ function ProjectForm({btnText}) {
                 <Select 
                     name="category_id"
                     text="Selecione a categoria"
+                    options={categories}
                 />
                 <SubmitButton text={btnText}></SubmitButton>
 
