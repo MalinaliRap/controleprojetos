e2df942d336ad79c0c2f4dae7e0c620d02e80e09
MalinaliRap <utfprdiario@gmail.com>
2023-05-10 23:17:42 -0300

    Aula #26

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import {useLocation} from 'react-router-dom';
 import Message from '../layout/Message';
+import styles from './Projects.module.css';
+import Container from '../layout/Container';
+import LinkButton from '../layout/LinkButton';
 
 function Projects(props) {
 
@@ -10,11 +13,17 @@ if(location.state){
 }
 
     return (
-       <div>
-            <h1>
-                Meus Projetos
-            </h1>
+       <div className={styles.project_container}>
+           <div  className={styles.title_container}>
+                <h1>
+                    Meus Projetos
+                </h1>
+                <LinkButton to="/newproject" text="Criar projeto"></LinkButton>
+           </div>
             {message &&  <Message msg='Alguma Mensagem' type='success'></Message>}
+            <Container customClass="start" >
+                <p>Projetos ...</p>
+            </Container>
        </div>
     );
 }
